d02cdc89dda3deefd70f1c745e3c461c6cce0a70
wgamagomes <wgamagomes@gmail.com>
2019-06-16 11:03:43 -0300

    [ADD] Added InfoValueObject.cs

---
+++ XPTO/DesignPatterns/Behavioral/ChainOfResponsibility/Xpto.Behavioral.ChainOfResponsibility/ValueObject/InfoValueObject.cs
@@ -0,0 +1,25 @@
+﻿using Xpto.Behavioral.ChainOfResponsibility.Enums;
+
+namespace Xpto.Behavioral.ChainOfResponsibility.ValueObject
+{
+    public class InfoValueObject
+    {
+        public string Name { get; }
+        public string LastName { get; }
+        public string Email { get; }
+        public string PhoneNumber { get; }
+        public string FullName { get { return $"{Name} {LastName}"; } }
+        public Status Status { get; }
+
+        public InfoValueObject(string name, string lastName, string email, string phoneNumber, Status status)
+        {
+            Name = name;
+            LastName = lastName;
+            Email = email;
+            PhoneNumber = phoneNumber;
+            Status = status;
+        }
+
+        public string Message { get; set; }
+    }
+}
