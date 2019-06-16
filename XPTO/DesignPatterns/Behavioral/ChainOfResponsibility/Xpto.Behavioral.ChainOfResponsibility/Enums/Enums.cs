fa8773bef26a9e488959dc64b6c42051bba296bb
wgamagomes <wgamagomes@gmail.com>
2019-06-16 10:58:14 -0300

    [ADD] Added Enums.cs

---
+++ XPTO/DesignPatterns/Behavioral/ChainOfResponsibility/Xpto.Behavioral.ChainOfResponsibility/Enums/Enums.cs
@@ -0,0 +1,15 @@
+﻿namespace Xpto.Behavioral.ChainOfResponsibility.Enums
+{
+    public enum NotificationType
+    {
+        Email,
+        SMS
+    }
+
+    public enum Status
+    {
+        SellerRefusal,
+        CustomerRefusal,
+        CustomerAcceptance
+    }
+}
