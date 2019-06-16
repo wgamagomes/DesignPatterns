ec910c393e292b7b5a223780eed86b2e0365cb4f
wgamagomes <wgamagomes@gmail.com>
2019-06-16 11:05:41 -0300

    [ADD] Added EmailNotification.cs

---
+++ XPTO/DesignPatterns/Behavioral/ChainOfResponsibility/Xpto.Behavioral.ChainOfResponsibility/Notification/EmailNotification.cs
@@ -0,0 +1,19 @@
+﻿using Xpto.Behavioral.ChainOfResponsibility.Enums;
+using Xpto.Behavioral.ChainOfResponsibility.ValueObject;
+
+namespace Xpto.Behavioral.ChainOfResponsibility.Notification
+{
+    public class EmailNotification : BaseNotification
+    {
+        public EmailNotification()
+            : base(NotificationType.Email)
+        {
+
+        }
+
+        public override void Notify(InfoValueObject info)
+        {
+            //Your email Notification
+        }
+    }
+}
