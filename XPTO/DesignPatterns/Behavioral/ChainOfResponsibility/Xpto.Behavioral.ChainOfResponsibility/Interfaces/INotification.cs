6d43ba3c7f8c192d88e671b32121325af30fafb6
wgamagomes <wgamagomes@gmail.com>
2019-06-16 11:08:41 -0300

    [ADD] Added INotification.cs

---
+++ XPTO/DesignPatterns/Behavioral/ChainOfResponsibility/Xpto.Behavioral.ChainOfResponsibility/Interfaces/INotification.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Xpto.Behavioral.ChainOfResponsibility.ValueObject;
+
+namespace Xpto.Behavioral.ChainOfResponsibility.Interfaces
+{
+    public interface INotification
+    {
+        INotification Next { get; set; }
+
+        void Notify(Action<InfoValueObject> execute, InfoValueObject info);
+    }
+}
