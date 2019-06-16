09b2f2bb1f6067e7b46818d5040b8d2322a19fdc
wgamagomes <wgamagomes@gmail.com>
2019-06-16 11:04:54 -0300

    [ADD] Added BaseNotification.cs

---
+++ XPTO/DesignPatterns/Behavioral/ChainOfResponsibility/Xpto.Behavioral.ChainOfResponsibility/Notification/BaseNotification.cs
@@ -0,0 +1,18 @@
+﻿using Xpto.Behavioral.ChainOfResponsibility.Enums;
+using Xpto.Behavioral.ChainOfResponsibility.ValueObject;
+
+namespace Xpto.Behavioral.ChainOfResponsibility.Notification
+{
+    public  abstract class BaseNotification
+    {
+        public BaseNotification(NotificationType notificationType)
+        {
+            NotificationType = notificationType;
+        }
+
+        public NotificationType NotificationType { get; }
+
+        public abstract void Notify(InfoValueObject info);
+
+    }
+}
