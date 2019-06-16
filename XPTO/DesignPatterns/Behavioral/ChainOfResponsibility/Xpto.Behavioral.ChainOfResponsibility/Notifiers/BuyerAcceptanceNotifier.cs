091909d85375be4c0c98727116e8cedede647548
wgamagomes <wgamagomes@gmail.com>
2019-06-16 11:27:07 -0300

    [ADD] Added BuyerAcceptanceNotifier

---
+++ XPTO/DesignPatterns/Behavioral/ChainOfResponsibility/Xpto.Behavioral.ChainOfResponsibility/Notifiers/BuyerAcceptanceNotifier.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Xpto.Behavioral.ChainOfResponsibility.Enums;
+using Xpto.Behavioral.ChainOfResponsibility.Interfaces;
+using Xpto.Behavioral.ChainOfResponsibility.ValueObject;
+
+namespace Xpto.Behavioral.ChainOfResponsibility.Notifiers
+{
+    public class BuyerAcceptanceNotifier : INotifier
+    {
+        public INotifier Next { get; set; }
+
+        public void Notify(Action<InfoValueObject> execute, InfoValueObject info)
+        {
+            if (info.Status == Status.CustomerAcceptance)
+            {
+                info.Message = $"Buyer Accepted your offer.";
+                execute(info);
+            }
+            else
+            {
+                Next?.Notify(execute, info);
+            }
+        }
+    }
+}
