b699c3a9ae160d5998b6b801b580d65d1055edb8
wgamagomes <wgamagomes@gmail.com>
2019-06-16 11:34:23 -0300

    [ADD] Added BuyerRefusalNotifier.cs

---
+++ XPTO/DesignPatterns/Behavioral/ChainOfResponsibility/Xpto.Behavioral.ChainOfResponsibility/Notifiers/BuyerRefusalNotifier.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Xpto.Behavioral.ChainOfResponsibility.Enums;
+using Xpto.Behavioral.ChainOfResponsibility.Interfaces;
+using Xpto.Behavioral.ChainOfResponsibility.ValueObject;
+
+namespace Xpto.Behavioral.ChainOfResponsibility.Notifiers
+{
+    public class BuyerRefusalNotifier : INotifier
+    {
+        public INotifier Next { get; set; }
+           
+        public void Notify(Action<InfoValueObject> execute, InfoValueObject info)
+        {
+            if (info.Status == Status.CustomerRefusal)
+            {
+                info.Message = "Buyer Refuses your offer.";
+                execute(info);
+            }
+            else
+            {
+                Next?.Notify(execute, info);
+            }
+        }
+    }
+}
