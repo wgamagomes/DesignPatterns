417faddf6a856f604b196812be948248d71f7c35
wgamagomes <wgamagomes@gmail.com>
2019-06-16 11:06:23 -0300

    [ADD] Added SMSNotification.cs

---
+++ XPTO/DesignPatterns/Behavioral/ChainOfResponsibility/Xpto.Behavioral.ChainOfResponsibility/Notification/SMSNotification.cs
@@ -0,0 +1,18 @@
+﻿using Xpto.Behavioral.ChainOfResponsibility.Enums;
+using Xpto.Behavioral.ChainOfResponsibility.ValueObject;
+
+namespace Xpto.Behavioral.ChainOfResponsibility.Notification
+{
+    public class SMSNotification : BaseNotification
+    {
+        public SMSNotification()
+            : base(NotificationType.SMS)
+        {
+
+        }
+        public override void Notify(InfoValueObject info)
+        {
+            //your SMS notification
+        }
+    }
+}
