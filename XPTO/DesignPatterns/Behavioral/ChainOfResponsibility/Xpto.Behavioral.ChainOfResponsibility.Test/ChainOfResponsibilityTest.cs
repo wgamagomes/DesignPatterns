05b23fda7e3e94cdb7f9f8d05cb9079f31632821
wgamagomes <wgamagomes@gmail.com>
2019-06-18 16:50:23 -0300

    [ADD] Added ChainOfResponsibilityTest.cs

---
+++ XPTO/DesignPatterns/Behavioral/ChainOfResponsibility/Xpto.Behavioral.ChainOfResponsibility.Test/ChainOfResponsibilityTest.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using Xpto.Behavioral.ChainOfResponsibility.Enums;
+using Xpto.Behavioral.ChainOfResponsibility.Notification;
+using Xpto.Behavioral.ChainOfResponsibility.ValueObject;
+
+namespace Xpto.Behavioral.ChainOfResponsibility.Test
+{
+    public class ChainOfResponsibilityTest
+    {
+        [Test]
+        public void Test()
+        {  
+            NotificationInitializer.Initialize(new EmailNotification(), new InfoValueObject("Ton", "Gomes", "tg@gmail.com", "", Status.CustomerAcceptance));
+        }
+    }
+}
