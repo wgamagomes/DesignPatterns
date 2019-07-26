675c8f88311c826e4a4d1539b3a8e7ebf87dac6c
wgamagomes <wgamagomes@gmail.com>
2019-07-26 14:01:14 -0300

    [ADD] Specification pattern project

---
+++ XPTO/DesignPatterns/SpecificationPattern/Xpto.SpecificationPattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Xpto.SpecificationPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
