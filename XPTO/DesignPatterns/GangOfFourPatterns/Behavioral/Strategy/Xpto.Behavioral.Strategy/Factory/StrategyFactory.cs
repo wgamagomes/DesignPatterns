7d627a646c6bbae523c1ba1e8c6a9c2a530c40d4
wgamagomes <wgamagomes@gmail.com>
2019-08-16 19:59:46 -0300

    [ADD] Added StrategyFactory class

---
+++ XPTO/DesignPatterns/GangOfFourPatterns/Behavioral/Strategy/Xpto.Behavioral.Strategy/Factory/StrategyFactory.cs
@@ -0,0 +1,33 @@
+﻿using Xpto.Behavioral.Strategy.Events;
+using Xpto.Behavioral.Strategy.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Xpto.Behavioral.Strategy.Factory
+{
+    public  class StrategyFactory
+    {
+        private static List<IStrategy> _strategies;
+        
+        public IStrategy this[StrategyType key]
+        {
+            get
+            {
+                if (_strategies == null)
+                {
+                    _strategies = AppDomain
+                        .CurrentDomain
+                        .GetAssemblies()
+                        .SelectMany(assembly => assembly.GetTypes().Where(type => type.IsClass && !type.IsAbstract && typeof(IStrategy).IsAssignableFrom(type))
+                        .Select(strategy => (IStrategy)Activator.CreateInstance(strategy)))
+                        .ToList();
+                }
+
+                return _strategies
+                  .Where(s => s.StrategyType == key)
+                  .FirstOrDefault();
+            }
+        }
+    }
+}
