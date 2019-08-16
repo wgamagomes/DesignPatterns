37a8f065218da2ae0d7a0333f56019a17b3e64f0
wgamagomes <wgamagomes@gmail.com>
2019-08-16 19:56:48 -0300

    [ADD] Added Event class

---
+++ XPTO/DesignPatterns/GangOfFourPatterns/Behavioral/Strategy/Xpto.Behavioral.Strategy/Events/Event.cs
@@ -0,0 +1,14 @@
+﻿namespace Xpto.Behavioral.Strategy.Events
+{
+    public class Event
+    {
+        public StrategyType StrategyType { get; set; }
+    }
+
+    public enum StrategyType
+    {
+        Basic,
+        Intermediate,
+        Advanced
+    }
+}
