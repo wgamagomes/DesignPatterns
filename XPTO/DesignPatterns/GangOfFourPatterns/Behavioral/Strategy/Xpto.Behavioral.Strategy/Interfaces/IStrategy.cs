79bfd0a622442f1010729fddb5000646e5363e2f
wgamagomes <wgamagomes@gmail.com>
2019-08-16 19:57:49 -0300

    [ADD] Added IStrategy interface

---
+++ XPTO/DesignPatterns/GangOfFourPatterns/Behavioral/Strategy/Xpto.Behavioral.Strategy/Interfaces/IStrategy.cs
@@ -0,0 +1,10 @@
+﻿using Xpto.Behavioral.Strategy.Events;
+
+namespace Xpto.Behavioral.Strategy.Interfaces
+{
+    public interface IStrategy
+    {
+        StrategyType StrategyType { get; }
+        void Exec(Event @event);
+    }
+}
