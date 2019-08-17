1ac364408b1d00827c7807b82ff26bf876613464
wgamagomes <wgamagomes@gmail.com>
2019-08-17 15:13:04 -0300

    [ADD] Added EnemyShip abstract class

---
+++ XPTO/DesignPatterns/GangOfFourPatterns/Creational/Factory/Xpto.Creational.Factory/EnemyShip.cs
@@ -0,0 +1,24 @@
+﻿namespace Xpto.Creational.Factory
+{
+    public abstract class EnemyShip
+    {
+        public string Name { get; private set; }
+
+        public double Damage { get; private set; }
+
+        public string FollowHeroShip()
+        {
+            return $"{Name} is following the hero";
+        }
+
+        public string DisplayEnemyShip()
+        {
+            return $"{Name} is on the screen";
+        }
+
+        public string EnemyShipShoots()
+        {
+            return $"{Name} attacks and does { Damage} damage to hero";
+        }
+    }
+}
