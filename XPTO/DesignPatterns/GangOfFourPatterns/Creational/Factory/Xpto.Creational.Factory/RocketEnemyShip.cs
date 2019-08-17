01424c86d63b7e30b9a297c3c771b40208210f02
wgamagomes <wgamagomes@gmail.com>
2019-08-17 15:18:16 -0300

    [ADD] Added RocketEnemyShip class

---
+++ XPTO/DesignPatterns/GangOfFourPatterns/Creational/Factory/Xpto.Creational.Factory/RocketEnemyShip.cs
@@ -0,0 +1,11 @@
+﻿namespace Xpto.Creational.Factory
+{
+    public class RocketEnemyShip : EnemyShip
+    {
+        public RocketEnemyShip()
+        {
+            Name = "Rocket Enemy Ship";
+            Damage = 10.0;
+        }
+    }
+}
