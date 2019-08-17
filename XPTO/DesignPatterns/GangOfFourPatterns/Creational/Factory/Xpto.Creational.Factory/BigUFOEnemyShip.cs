23e7b71e1a63700a24e677dc19c8c923221ffe99
wgamagomes <wgamagomes@gmail.com>
2019-08-17 15:20:58 -0300

    [ADD] Added BigUFOEnemyShip class

---
+++ XPTO/DesignPatterns/GangOfFourPatterns/Creational/Factory/Xpto.Creational.Factory/BigUFOEnemyShip.cs
@@ -0,0 +1,11 @@
+﻿namespace Xpto.Creational.Factory
+{
+    public class BigUFOEnemyShip : UFOEnemyShip
+    {
+        public BigUFOEnemyShip()
+        {
+            Name = "Big UFO Enemy Ship";
+            Damage = 40.0;
+        }
+    }
+}
