046a28afd05dabcf7e8a473bddbaf76369cffedd
wgamagomes <wgamagomes@gmail.com>
2019-08-20 20:06:13 -0300

    [ADD] Added ProductB1 and ProductB2 classes

---
+++ XPTO/DesignPatterns/GangOfFourPatterns/Creational/Factory/Xpto.Creational.AbstractFactory/ProductB1.cs
@@ -0,0 +1,10 @@
+﻿namespace Xpto.Creational.AbstractFactory
+{
+    public class ProductB1 : ProductBBase
+    {
+        public override void Interact(ProductABase product)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ XPTO/DesignPatterns/GangOfFourPatterns/Creational/Factory/Xpto.Creational.AbstractFactory/ProductB2.cs
@@ -0,0 +1,10 @@
+﻿namespace Xpto.Creational.AbstractFactory
+{
+    public class ProductB2 : ProductBBase
+    {
+        public override void Interact(ProductABase product)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
