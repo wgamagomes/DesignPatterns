d63d4609e004d0514b7d121e837ac4562e5b5f13
wgamagomes <wgamagomes@gmail.com>
2019-08-20 19:58:27 -0300

    [ADD] Added IFactory interface [ADD] Added ProductA and ProductB classes

---
+++ XPTO/DesignPatterns/GangOfFourPatterns/Creational/Factory/Xpto.Creational.AbstractFactory/IFactory.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xpto.Creational.AbstractFactory
+{
+    class AbstractFactory
+    {
+    }
+}
---
+++ XPTO/DesignPatterns/GangOfFourPatterns/Creational/Factory/Xpto.Creational.AbstractFactory/ProductA.cs
@@ -0,0 +1,6 @@
+﻿namespace Xpto.Creational.AbstractFactory
+{
+    public class ProductA
+    {
+    }
+}
