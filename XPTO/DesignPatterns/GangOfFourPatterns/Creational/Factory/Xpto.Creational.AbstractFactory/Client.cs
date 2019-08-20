cc12a6cd9a0f0c6656c8f2aa0efdbadf64cdd570
wgamagomes <wgamagomes@gmail.com>
2019-08-20 20:14:25 -0300

    [ADD] Added Client class

---
+++ XPTO/DesignPatterns/GangOfFourPatterns/Creational/Factory/Xpto.Creational.AbstractFactory/Client.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xpto.Creational.AbstractFactory
+{
+    public class Client
+    {
+        private readonly ProductABase _productA;
+        private readonly ProductBBase _productB;
+
+        public Client(IFactory factory)
+        {
+            _productA = factory.CreateProductA();
+            _productB = factory.CreateProductB();
+        }
+
+        public string Run()
+        {
+            return _productB.Interact(_productA);
+        }
+    }
+}
