a191bb6648842ab795951bbd784ce2e0c2977e27
wgamagomes <wgamagomes@gmail.com>
2019-08-21 10:28:14 -0300

    [ADD] Added ExpressionSpecification  class

---
+++ XPTO/DesignPatterns/SpecificationPattern/Xpto.SpecificationPattern/Xpto.SpecificationPattern/Specifications/ExpressionSpecification.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xpto.SpecificationPattern.Specifications.Operators;
+
+namespace Xpto.SpecificationPattern.Specifications
+{
+    public class ExpressionSpecification<T> : CompositeSpecification<T>
+    {
+        private readonly Func<T, bool> _expression;
+
+        public ExpressionSpecification(Func<T, bool> expression)
+        {
+            _expression = expression ?? throw new ArgumentException();
+        }
+
+        public override bool IsSatisfiedBy(T candidate) =>
+            _expression(candidate);
+    }
+}
