ac6e5437f368f92adf9562586a151a2e28e76cb3
wgamagomes <wgamagomes@gmail.com>
2019-08-21 10:20:56 -0300

    [ADD] Added ISpecification interface

---
+++ XPTO/DesignPatterns/SpecificationPattern/Xpto.SpecificationPattern/Xpto.SpecificationPattern/Specifications/Interfaces/ISpecification.cs
@@ -0,0 +1,17 @@
+﻿namespace Xpto.SpecificationPattern.Specifications.Interfaces
+{
+    public interface ISpecification<T>
+    {
+        bool IsSatisfiedBy(T candidate);
+
+        ISpecification<T> And(ISpecification<T> specification);
+
+        ISpecification<T> AndNot(ISpecification<T> specification);
+
+        ISpecification<T> Or(ISpecification<T> specification);
+
+        ISpecification<T> OrNot(ISpecification<T> specification);
+
+        ISpecification<T> Not(ISpecification<T> specification);
+    }
+}
