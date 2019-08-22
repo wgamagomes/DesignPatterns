2ff58eb210506d4a54b90230acaca23aefbdded8
wgamagomes <wgamagomes@gmail.com>
2019-08-22 13:15:24 -0300

    [ADD] Added MovieBuilder class

---
+++ XPTO/DesignPatterns/SpecificationPattern/Xpto.SpecificationPattern/Xpto.SpecificationPattern.Test/MovieBuilder.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Xpto.SpecificationPattern.Test
+{
+    public class MovieBuilder
+    {
+        private Movie _movie;
+
+        public MovieBuilder()
+        {
+            _movie = new Movie();
+        }
+        public MovieBuilder WithName(string name)
+        {
+            _movie.Name = name;
+            return this;
+        }
+
+        public MovieBuilder WithReleaseDate(DateTime releaseDate)
+        {
+            _movie.ReleaseDate = releaseDate;
+            return this;
+        }
+
+        public MovieBuilder WithMpaaRating(MpaaRating mpaaRating)
+        {
+            _movie.MpaaRating = mpaaRating;
+            return this;
+        }
+
+        public MovieBuilder WithGenre(string genre)
+        {
+            _movie.Genre = genre;
+            return this;
+        }
+
+        public MovieBuilder WithRating(double rating)
+        {
+            _movie.Rating = rating;
+            return this;
+        }
+
+        public Movie Build()
+        {
+            return _movie;
+        }
+    }
+
+}
