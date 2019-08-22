7ff9c32246099149e7149d0ae71375332bcf21bd
wgamagomes <wgamagomes@gmail.com>
2019-08-22 13:21:21 -0300

    [ADD] Added SpecificationPatternTest class

---
+++ XPTO/DesignPatterns/SpecificationPattern/Xpto.SpecificationPattern/Xpto.SpecificationPattern.Test/SpecificationPatternTest.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Xpto.SpecificationPattern.Test
+{
+    public class SpecificationPatternTest
+    {
+
+        private static IEnumerable<TestCaseData> TestCaseData1
+        {
+            get
+            {
+                var builder = new MovieBuilder()
+                     .WithMpaaRating(MpaaRating.PG13);
+
+                yield return new TestCaseData(builder.Build(), false);
+
+                builder = new MovieBuilder()
+                     .WithMpaaRating(MpaaRating.G);
+
+                yield return new TestCaseData(builder.Build(), true);
+
+                builder = new MovieBuilder()
+                     .WithMpaaRating(MpaaRating.R);
+
+                yield return new TestCaseData(builder.Build(), false);
+
+            }
+        }
+    }
+}
