ee6b3be6c4e4760d825d3e9a6b9846083c0ee21f
wgamagomes <wgamagomes@gmail.com>
2019-08-22 12:53:57 -0300

    [ADD] Added Movie class

---
+++ XPTO/DesignPatterns/SpecificationPattern/Xpto.SpecificationPattern/Xpto.SpecificationPattern.Test/Movie.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Xpto.SpecificationPattern.Test
+{
+    public class Movie
+    {
+        public string Name { get; }
+
+        public DateTime ReleaseDate { get; }
+
+        public MpaaRating MpaaRating { get; }
+
+        public string Genre { get; }
+
+        public double Rating { get; }
+    }
+
+    public enum MpaaRating
+
+    {
+        G,
+        PG13,
+        R
+    }
+}
