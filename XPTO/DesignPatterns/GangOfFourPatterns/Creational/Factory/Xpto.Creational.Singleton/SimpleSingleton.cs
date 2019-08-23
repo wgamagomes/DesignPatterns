8b20ed8a6264d9794fd86168934236974cc5e504
wgamagomes <wgamagomes@gmail.com>
2019-08-23 15:28:17 -0300

    [ADD] implemented SimpleSingleton class

--- XPTO/DesignPatterns/GangOfFourPatterns/Creational/Factory/Xpto.Creational.Singleton/SimpleSingleton.cs
+++ XPTO/DesignPatterns/GangOfFourPatterns/Creational/Factory/Xpto.Creational.Singleton/SimpleSingleton.cs
@@ -2,5 +2,20 @@
 {
     public class SimpleSingleton
     {
+        private static SimpleSingleton _instance;
+        private static readonly object _lock = new object();
+
+        public static SimpleSingleton Instance()
+        {
+            lock (_lock)
+            {
+                if (_instance == null)
+                {
+                    _instance = new SimpleSingleton();
+                }
+            }
+
+            return _instance;
+        }
     }
 }
