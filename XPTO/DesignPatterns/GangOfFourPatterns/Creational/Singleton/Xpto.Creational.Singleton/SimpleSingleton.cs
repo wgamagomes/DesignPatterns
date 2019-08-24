b35f6986ebfe2e0b6a592ad24ab803f96d4cffce
wgamagomes <wgamagomes@gmail.com>
2019-08-24 19:43:42 -0300

    [ALT] Alter singleton projects directory

---
+++ XPTO/DesignPatterns/GangOfFourPatterns/Creational/Singleton/Xpto.Creational.Singleton/SimpleSingleton.cs
@@ -0,0 +1,29 @@
+﻿namespace Xpto.Creational.Singleton
+{
+    /// <summary>
+    /// Essentially, a singleton is a class which only allows a single 
+    /// instance of itself to be created, and usually gives simple access 
+    /// to that instance. 
+    /// https://csharpindepth.com/Articles/Singleton
+    /// </summary>
+    public class SimpleSingleton
+    {
+        private static SimpleSingleton _instance;
+        private static readonly object _lock = new object();
+
+        public static SimpleSingleton Instance()
+        {
+            if (_instance == null)
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new SimpleSingleton();
+                    }
+                }
+            }
+                return _instance;
+        }
+    }
+}
