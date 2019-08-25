7c9fc2ca46cfea948ea175a27b341511884ddd1b
wgamagomes <wgamagomes@gmail.com>
2019-08-25 10:56:49 -0300

    [ADD] Added Command pattern projects

---
+++ XPTO/DesignPatterns/GangOfFourPatterns/Behavioral/Command/Xpto.Behavioral.Command.Test/CommandTest.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Xpto.GangOfFourPatterns.Behavioral.Command.Test
+{
+    public class CommandTest
+    {   
+
+        [SetUp]
+        public void Setup()
+        {
+           
+        }
+
+        [Test]
+        public void Should_Do_Something()
+        {
+           
+        }       
+    }
+}
--- XPTO/DesignPatterns/GangOfFourPatterns/Creational/Singleton/Xpto.Creational.Singleton.Test/SingletonTest.cs
+++ XPTO/DesignPatterns/GangOfFourPatterns/Creational/Singleton/Xpto.Creational.Singleton.Test/SingletonTest.cs
@@ -2,6 +2,6 @@
 {
     public class SingletonTest
     {
-
+        //TODO: implement your tests here
     }
 }
