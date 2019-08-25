1cd7b63aed769d813a6f2e4e99b15b627ef8c0a3
wgamagomes <wgamagomes@gmail.com>
2019-08-25 11:03:13 -0300

    [ADD] Added Command and Receiver classes

---
+++ XPTO/DesignPatterns/GangOfFourPatterns/Behavioral/Command/Xpto.Behavioral.Command/Base/Command.cs
@@ -0,0 +1,15 @@
+﻿namespace Xpto.Behavioral.Command.Base
+{
+    public abstract class Command
+
+    {
+        protected Receiver receiver;
+
+        public Command(Receiver receiver)
+        {
+            this.receiver = receiver;
+        }
+
+        public abstract void Execute();
+    }
+}
