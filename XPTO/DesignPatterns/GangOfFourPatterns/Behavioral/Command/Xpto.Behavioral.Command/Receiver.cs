82f88c4c291a9be6d6efd331a52eca1ffd599932
wgamagomes <wgamagomes@gmail.com>
2019-08-26 11:12:04 -0300

    [ADD] Implemented Receiver class

--- XPTO/DesignPatterns/GangOfFourPatterns/Behavioral/Command/Xpto.Behavioral.Command/Receiver.cs
+++ XPTO/DesignPatterns/GangOfFourPatterns/Behavioral/Command/Xpto.Behavioral.Command/Receiver.cs
@@ -1,10 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Xpto.Behavioral.Command
+﻿namespace Xpto.Behavioral.Command
 {
     public class Receiver
     {
+        public string  Action()
+        {
+           return "Called Receiver.Action()";
+        }
     }
 }
