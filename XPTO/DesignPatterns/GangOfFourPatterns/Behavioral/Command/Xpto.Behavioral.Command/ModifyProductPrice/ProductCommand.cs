1ad43739ff4f5537a1d7f2569ce26430d11ad749
wgamagomes <wgamagomes@gmail.com>
2019-08-27 09:34:45 -0300

    [ADD] Added ProductCommand class

---
+++ XPTO/DesignPatterns/GangOfFourPatterns/Behavioral/Command/Xpto.Behavioral.Command/ModifyProductPrice/ProductCommand.cs
@@ -0,0 +1,27 @@
+﻿namespace Xpto.Behavioral.Command.ModifyProductPrice
+{
+    public class ProductCommand : ICommand
+    {
+        private readonly Product _product;
+        private readonly PriceAction _priceAction;
+        private readonly int _amount;
+
+        public ProductCommand(Product product, PriceAction priceAction, int amount)
+        {
+            _product = product;
+            _priceAction = priceAction;
+            _amount = amount;
+        }
+
+        public void ExecuteAction()
+        {
+            if (_priceAction == PriceAction.Increase)
+            {
+                _product.IncreasePrice(_amount);
+                return;
+            }
+
+            _product.DecreasePrice(_amount);            
+        }
+    }
+}
