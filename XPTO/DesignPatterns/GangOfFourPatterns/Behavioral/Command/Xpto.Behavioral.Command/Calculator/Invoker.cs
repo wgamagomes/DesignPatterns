d627a1db8886400d7a0be90d1980ba2eca0ce12c
wgamagomes <wgamagomes@gmail.com>
2019-08-28 17:25:58 -0300

    [ADD] Added Invoker class

---
+++ XPTO/DesignPatterns/GangOfFourPatterns/Behavioral/Command/Xpto.Behavioral.Command/Calculator/Invoker.cs
@@ -0,0 +1,10 @@
+﻿namespace Xpto.Behavioral.Command.Calculator
+{
+    public class Invoker
+    {
+        public int ExecuteCommand(ICommand command)
+        {
+            return command.Execute();
+        }
+    }
+}
