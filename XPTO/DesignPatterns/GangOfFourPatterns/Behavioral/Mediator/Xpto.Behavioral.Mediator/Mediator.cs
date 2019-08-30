c048cd8ad4252079e2d981d0a93b83f40ec22d6f
wgamagomes <wgamagomes@gmail.com>
2019-08-30 17:38:49 -0300

    [ADD] Implemented Mediator class

--- XPTO/DesignPatterns/GangOfFourPatterns/Behavioral/Mediator/Xpto.Behavioral.Mediator/Mediator.cs
+++ XPTO/DesignPatterns/GangOfFourPatterns/Behavioral/Mediator/Xpto.Behavioral.Mediator/Mediator.cs
@@ -2,5 +2,20 @@
 {
     public class Mediator
     {
+        public delegate void Callback(string message, string from);
+        Callback respond;
+        public void SignOn(Callback method)
+        {
+            respond += method;
+        }
+        public void Block(Callback method)
+        {
+            respond -= method;
+        }
+        public void Unblock(Callback method)
+        {
+            respond += method;
+        }
+        // Send is implemented as a broadcast 30         public void Send(string message, string from) { 31           respond(message, from); 32           Console.WriteLine(); 33         } 
     }
 }
