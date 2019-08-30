fd4b727746efe141efd33891af5a5ee1e9a86bc3
wgamagomes <wgamagomes@gmail.com>
2019-08-30 18:24:48 -0300

    [ADD] Implemented Colleague class

--- XPTO/DesignPatterns/GangOfFourPatterns/Behavioral/Mediator/Xpto.Behavioral.Mediator/Colleague.cs
+++ XPTO/DesignPatterns/GangOfFourPatterns/Behavioral/Mediator/Xpto.Behavioral.Mediator/Colleague.cs
@@ -1,6 +1,25 @@
-﻿namespace Xpto.Behavioral.Mediator
+﻿using System;
+
+namespace Xpto.Behavioral.Mediator
 {
     public class Colleague
     {
+
+        Mediator mediator;
+        protected string name;
+        public Colleague(Mediator mediator, string name)
+        {
+            this.mediator = mediator;
+            mediator.SignOn(Receive);
+            this.name = name;
+        }
+        public virtual void Receive(string message, string from)
+        {
+            Console.WriteLine(name + " received from " + from + ": " + message);
+        }
+        public void Send(string message) { Console.WriteLine("Send (From " + name + "): " + message);
+
+           // mediator.Send(message, name);
+        }
     }
 }
