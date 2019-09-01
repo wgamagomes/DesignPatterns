04369028946cab207c448c1d9de51f4f47a6882c
wgamagomes <wgamagomes@gmail.com>
2019-09-01 12:56:20 -0300

    [ADD] Added Block message test method

--- XPTO/DesignPatterns/GangOfFourPatterns/Behavioral/Mediator/Xpto.Behavioral.Mediator.Test/MediatorTest.cs
+++ XPTO/DesignPatterns/GangOfFourPatterns/Behavioral/Mediator/Xpto.Behavioral.Mediator.Test/MediatorTest.cs
@@ -25,5 +25,19 @@ namespace Xpto.Behavioral.Mediator.Test
             branch1.Send("Ack"); // by design does not get a copy 
            
         }
+
+        [Test]
+        public void Should_Block_Messages()
+        {
+            CSharpThreeDesignPatternsBook.Mediator m = new CSharpThreeDesignPatternsBook.Mediator();// Two from head office and one from a branch office 
+            Colleague head1 = new Colleague(m, "John");
+            Colleague branch1 = new Colleague(m, "David");
+            Colleague head2 = new Colleague(m, "Lucy");
+
+            head1.Send("Meeting on Tuesday, please all ack");
+            branch1.Send("Ack"); // by design does not get a copy 
+            m.Block(branch1.Receive); // temporarily gets no messages 
+            head1.Send("Still awaiting some Acks");           
+        }
     }
 }
