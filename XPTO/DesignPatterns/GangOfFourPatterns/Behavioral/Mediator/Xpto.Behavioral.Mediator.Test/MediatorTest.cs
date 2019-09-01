1b6e8bd58e085e281a468b708e81255573876241
wgamagomes <wgamagomes@gmail.com>
2019-09-01 12:54:42 -0300

    [ADD] Added Test class  and Test method

---
+++ XPTO/DesignPatterns/GangOfFourPatterns/Behavioral/Mediator/Xpto.Behavioral.Mediator.Test/MediatorTest.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xpto.Behavioral.Mediator.CSharpThreeDesignPatternsBook;
+namespace Xpto.Behavioral.Mediator.Test
+{
+    class MediatorTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Should_Send_Message()
+        {
+            CSharpThreeDesignPatternsBook.Mediator m = new CSharpThreeDesignPatternsBook.Mediator();// Two from head office and one from a branch office 
+            Colleague head1 = new Colleague(m, "John");
+            Colleague branch1 = new Colleague(m, "David");
+            Colleague head2 = new Colleague(m, "Lucy");
+
+            head1.Send("Meeting on Tuesday, please all ack");
+            branch1.Send("Ack"); // by design does not get a copy 
+           
+        }
+    }
+}
