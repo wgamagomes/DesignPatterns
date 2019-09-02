2f0ec1c5513c75168d62030fc2efd54fd0aed1df
wgamagomes <wgamagomes@gmail.com>
2019-09-02 17:21:58 -0300

    [ADD] Added Own Mediator

---
+++ XPTO/DesignPatterns/GangOfFourPatterns/Behavioral/Mediator/Xpto.Behavioral.Mediator/Own/Mediator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xpto.Behavioral.Mediator.Own
+{
+    public class Mediator : IMediator
+    {
+        public void Publish(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
