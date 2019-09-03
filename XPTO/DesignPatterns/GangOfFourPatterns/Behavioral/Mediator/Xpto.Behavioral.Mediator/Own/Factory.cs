38a5295d11c7fbd01ca79e47a4b11e24f71ce175
wgamagomes <wgamagomes@gmail.com>
2019-09-03 10:35:15 -0300

    [ADD] Added Factory class

---
+++ XPTO/DesignPatterns/GangOfFourPatterns/Behavioral/Mediator/Xpto.Behavioral.Mediator/Own/Factory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Xpto.Behavioral.Mediator.Own
+{
+    public class Factory<THandler>
+    {
+        private static List<THandler> _handlers;
+
+        public static List<THandler> GetRequestHandlers
+        {
+            get
+            {
+                if (_handlers == null)
+                {
+                    _handlers = AppDomain
+                        .CurrentDomain
+                        .GetAssemblies()
+                        .SelectMany(assembly => assembly.GetTypes().Where(type => type.IsClass && !type.IsAbstract && typeof(THandler).IsAssignableFrom(type))
+                        .Select(enemyShip => (THandler)Activator.CreateInstance(enemyShip)))
+                        .ToList();
+                }
+
+                return _handlers;
+            }
+        }
+    }
+}
+
