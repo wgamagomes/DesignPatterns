a07a64eb6e07eb7d76b3ffa7b72e1f266731c5d9
wgamagomes <wgamagomes@gmail.com>
2019-09-05 19:15:55 -0300

    [ADD] Added DoSomething.cs class

---
+++ XPTO/DesignPatterns/GangOfFourPatterns/Behavioral/Mediator/Xpto.Behavioral.Mediator/Own/WithEventsAndDelegates/DoSomething.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xpto.Behavioral.Mediator.Own.WithEventsAndDelegates
+{
+    public class DoSomething 
+    {
+        private event EventHandler<DoSomethingEventArgs> _eventInvoked;
+
+        public DoSomething()
+        {
+           
+        }
+
+        public void Subscribe(EventHandler<DoSomethingEventArgs> eventHandler)
+        {
+            _eventInvoked += eventHandler;
+        }
+
+        public void UnSubscribe(EventHandler<DoSomethingEventArgs> eventHandler)
+        {
+            _eventInvoked -= eventHandler;
+        }
+
+        public void Send(object sender)
+        {
+           // _eventInvoked()
+        }
+    }
+}
