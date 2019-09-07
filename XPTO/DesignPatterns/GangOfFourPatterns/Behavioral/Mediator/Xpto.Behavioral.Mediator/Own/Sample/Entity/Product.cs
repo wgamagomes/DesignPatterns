2bdbb1a8bc5144512c16ca82d60cca058fa06dd1
wgamagomes <wgamagomes@gmail.com>
2019-09-07 11:03:49 -0300

    [ADD] Added Product class

---
+++ XPTO/DesignPatterns/GangOfFourPatterns/Behavioral/Mediator/Xpto.Behavioral.Mediator/Own/Sample/Entity/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xpto.Behavioral.Mediator.Own.Sample
+{
+    public class Product
+    {
+        public decimal Price { get; set; }
+
+        public Guid Id { get; set; }
+
+        public Product()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
