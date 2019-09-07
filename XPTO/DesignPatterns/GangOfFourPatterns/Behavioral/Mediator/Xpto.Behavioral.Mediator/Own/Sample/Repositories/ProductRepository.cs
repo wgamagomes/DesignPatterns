de21e6fd543dceeeb80a7d18f55efa096c00fab7
wgamagomes <wgamagomes@gmail.com>
2019-09-07 11:04:30 -0300

    [ADD] Added ProductRepository class

---
+++ XPTO/DesignPatterns/GangOfFourPatterns/Behavioral/Mediator/Xpto.Behavioral.Mediator/Own/Sample/Repositories/ProductRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Xpto.Behavioral.Mediator.Own.Sample.Repositories
+{
+    public class ProductRepository
+    {
+        public static List<Product> Products;
+
+        public ProductRepository()
+        {
+            Products = new List<Product>();
+        }
+
+        void Insert(Product product)
+        {
+            Products.Add(product); 
+        }
+
+        List<Product> Get(Func<Product,bool> predicate)
+        {
+            return Products
+                    .Where(predicate)
+                    .ToList();
+        }
+    }
+}
