11f4683f764ae570e9b0c8f01dc420017aa51317
wgamagomes <wgamagomes@gmail.com>
2019-09-06 21:59:55 -0300

    [ADD] Added OtherNotification class

---
+++ XPTO/DesignPatterns/GangOfFourPatterns/Behavioral/Mediator/Xpto.Behavioral.Mediator/Own/Sample/OtherNotification.cs
@@ -0,0 +1,12 @@
+﻿namespace Xpto.Behavioral.Mediator.Own
+{
+    public class OtherNotification : INotification
+    {
+        public string Message { get; private set; }
+
+        public OtherNotification(string message)
+        {
+            Message = message;
+        }
+    }
+}
