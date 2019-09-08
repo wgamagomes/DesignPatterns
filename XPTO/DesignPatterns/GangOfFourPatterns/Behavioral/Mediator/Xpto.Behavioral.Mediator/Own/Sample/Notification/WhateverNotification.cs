9a5c7e1c7194df89845a13ccfb0eda1012abd7b2
wgamagomes <wgamagomes@gmail.com>
2019-09-08 18:32:59 -0300

    [ADD] Added WhateverNotification class and WhateverNotificationHandler class

---
+++ XPTO/DesignPatterns/GangOfFourPatterns/Behavioral/Mediator/Xpto.Behavioral.Mediator/Own/Sample/Notification/WhateverNotification.cs
@@ -0,0 +1,12 @@
+﻿namespace Xpto.Behavioral.Mediator.Own
+{
+    public class WhateverNotification : INotification
+    {
+        public string Message { get; private set; }
+
+        public WhateverNotification(string message)
+        {
+            Message = message;
+        }
+    }
+}
---
+++ XPTO/DesignPatterns/GangOfFourPatterns/Behavioral/Mediator/Xpto.Behavioral.Mediator/Own/Sample/Notification/WhateverNotificationHandler.cs
@@ -0,0 +1,12 @@
+﻿namespace Xpto.Behavioral.Mediator.Own
+{
+    public class WhateverNotificationHandler : INotificationHandler<WhateverNotification>
+    {
+        public static string ReceivedMessage { get; private set; }
+        public WhateverNotificationHandler() { }
+        public void Handle(WhateverNotification notification)
+        {
+            ReceivedMessage = notification.Message;
+        }
+    }
+}
