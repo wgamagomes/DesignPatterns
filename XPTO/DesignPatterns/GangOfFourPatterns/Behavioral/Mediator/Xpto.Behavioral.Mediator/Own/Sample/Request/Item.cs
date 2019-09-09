a1f4f82de12736c89c49c95c89eeee4079682060
wgamagomes <wgamagomes@gmail.com>
2019-09-09 09:37:20 -0300

    [ADD] Added Item class

---
+++ XPTO/DesignPatterns/GangOfFourPatterns/Behavioral/Mediator/Xpto.Behavioral.Mediator/Own/Sample/Request/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Xpto.Behavioral.Mediator.Own.Sample.Request
+{
+    public class Item
+    {
+        public Guid Id { get; set; }
+        public Guid IdProduct { get; private set; }
+        public int Quantity { get; private set; }
+
+        public Item(Guid idProduct, int Quantity)
+        {
+            Id = Guid.NewGuid();
+            IdProduct = idProduct;
+            this.Quantity = Quantity;
+        }
+    }
+}
