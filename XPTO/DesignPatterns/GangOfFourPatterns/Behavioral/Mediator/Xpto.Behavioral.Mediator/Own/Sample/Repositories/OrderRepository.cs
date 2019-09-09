eb1dfad2d0d0987578fa6c211d42826acf458612
wgamagomes <wgamagomes@gmail.com>
2019-09-09 09:33:40 -0300

    [ADD] Added OrderRepository class

---
+++ XPTO/DesignPatterns/GangOfFourPatterns/Behavioral/Mediator/Xpto.Behavioral.Mediator/Own/Sample/Repositories/OrderRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xpto.Behavioral.Mediator.Own.Sample.Request;
+
+namespace Xpto.Behavioral.Mediator.Own.Sample.Repositories
+{
+    class OrderRepository
+    {
+        public static List<Order> Orders;
+
+        public OrderRepository()
+        {
+            Orders = new List<Order>();
+        }
+
+        public void Insert(Order Order)
+        {
+            Orders.Add(Order);
+        }
+
+        public List<Order> Get(Func<Order, bool> predicate)
+        {
+            return Orders
+                    .Where(predicate)
+                    .ToList();
+        }
+
+        public void Delete(Func<Order, bool> predicate)
+        {
+            var result = Orders
+                    .Where(predicate)
+                    .ToList();
+
+            foreach (var prod in result)
+            {
+                Orders.Remove(prod);
+            }
+        }
+    }
+}
