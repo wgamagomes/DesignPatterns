6c5c05a62da9a70a3d02147a54ee00054cf83b0d
wgamagomes <wgamagomes@gmail.com>
2019-09-09 09:38:02 -0300

    [ADD] Added Order class

---
+++ XPTO/DesignPatterns/GangOfFourPatterns/Behavioral/Mediator/Xpto.Behavioral.Mediator/Own/Sample/Request/Order.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Xpto.Behavioral.Mediator.Own.Sample.Request
+{
+    public class Order
+    {
+        public Guid Id { get; set; }
+
+        public List<Item> Items { get; private set; }
+
+        public Order()
+        {
+            Id = Guid.NewGuid();
+            Items = new List<Item>();
+        }
+
+        public void AddItem(Item item)
+        {
+            Items.Add(item);
+        }
+    }
+}
