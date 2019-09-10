c17a880f3971e74d1fec933f8d756078344f9219
wgamagomes <wgamagomes@gmail.com>
2019-09-10 09:19:04 -0300

    [ADD] Added Request/RequestHandler class

---
+++ XPTO/DesignPatterns/GangOfFourPatterns/Behavioral/Mediator/Xpto.Behavioral.Mediator/Own/Sample/Request/RequestHandler.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xpto.Behavioral.Mediator.Own.Interfaces;
+
+namespace Xpto.Behavioral.Mediator.Own.Sample
+{
+    public class RequestHandler//: IRequestHandler<>
+    {
+    }
+}
