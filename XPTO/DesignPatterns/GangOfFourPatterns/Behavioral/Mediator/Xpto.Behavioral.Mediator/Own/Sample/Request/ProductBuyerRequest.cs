41043b9d247b5d7b3ed59a290025308a920e56be
wgamagomes <wgamagomes@gmail.com>
2019-09-10 09:18:21 -0300

    [ADD] Added ProductBuyerRequest class

---
+++ XPTO/DesignPatterns/GangOfFourPatterns/Behavioral/Mediator/Xpto.Behavioral.Mediator/Own/Sample/Request/ProductBuyerRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xpto.Behavioral.Mediator.Own.Interfaces;
+
+namespace Xpto.Behavioral.Mediator.Own.Sample.Request
+{
+    public class ProductBuyerRequest : IRequest
+    {
+        public Guid ProductId { get; private set; }
+
+        public int Quantity { get; private set; }
+        public ProductBuyerRequest(Guid productId, int quantity)
+        {
+            ProductId = productId;
+            Quantity = quantity;
+        }
+    }
+}
