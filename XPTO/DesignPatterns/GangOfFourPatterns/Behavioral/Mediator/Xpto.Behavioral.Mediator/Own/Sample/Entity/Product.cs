3c5a02be2434868554ec9b72337feeda9978e045
wgamagomes <wgamagomes@gmail.com>
2019-09-13 09:22:38 -0300

    [ADD] Implemented  Product class

--- XPTO/DesignPatterns/GangOfFourPatterns/Behavioral/Mediator/Xpto.Behavioral.Mediator/Own/Sample/Entity/Product.cs
+++ XPTO/DesignPatterns/GangOfFourPatterns/Behavioral/Mediator/Xpto.Behavioral.Mediator/Own/Sample/Entity/Product.cs
@@ -1,20 +1,58 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Xpto.Behavioral.Mediator.Own.Sample
 {
     public class Product
     {
-        public decimal Price { get; set; }
+        public double Price { get; set; }
 
-        public string Description { get; set; }
+        public string Description { get; private set; }
 
         public Guid Id { get; set; }
 
-        public Product()
+        public int Quantity { get; private set; }
+
+        public Product(string description, int quantity, double price)
         {
             Id = Guid.NewGuid();
+            Description = description;
+            Quantity = quantity;
+            Price = price;
+        }
+
+        public void AlterPrice(double price)
+        {
+            Price = price;
+        }
+
+        public void AlterDescription(string description)
+        {
+            Description = description;
+        }
+
+        public int IncreaseQuantity(int increment)
+        {
+            return Quantity += increment;
+        }
+
+        internal static bool Available(Product product)
+        {
+            return product.Quantity > 0;
+        }
+
+        internal static bool IsValid(Product product)
+        {
+            return product != null;
+        }
+
+        public int DecreaseQuantity(int decrement)
+        {
+            if (Quantity - decrement < 0)
+            {
+                throw new ArgumentException("Decrease quantity error");
+            }
+
+            return Quantity -= decrement;
         }
     }
 }
