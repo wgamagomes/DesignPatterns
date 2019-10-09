d7b2dbadd3a292f288e670c09588d96f6f80474a
wgamagomes <wgamagomes@gmail.com>
2019-10-09 15:44:01 -0300

    .

---
+++ XPTO/Mongo/Infra.Data/Repository/GenericRepository.cs
@@ -0,0 +1,44 @@
+﻿using Infra.Data.Interfaces;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Infra.Data.Repository
+{
+    public class GenericRepository<TDocument> : IGenericRepository<TDocument> where TDocument : Document
+    {  
+        private IMongoCollection<TDocument> collection;
+
+        public GenericRepository(IDbContext dbContext)
+        {  
+            this.collection = dbContext.DbSet<TDocument>();
+        }
+
+        public Task InsertAsync(TDocument document)
+        {
+            return collection.InsertOneAsync(document);
+        }
+
+        public Task DeleteAsync(Expression<Func<TDocument, bool>> filter)
+        {
+            return collection.FindOneAndDeleteAsync(filter);
+        }
+
+        public Task<TDocument> UpdateAsync(TDocument document)
+        {
+           return collection.FindOneAndReplaceAsync(d => d.Id == document.Id, document);           
+        }
+
+        public Task<List<TDocument>> FindAsync()
+        {
+            return collection.FindAsync(FilterDefinition<TDocument>.Empty).GetAwaiter().GetResult().ToListAsync();
+        }
+        public Task<List<TDocument>> FindAsync(Expression<Func<TDocument, bool>> filter)
+        {
+            return collection.FindAsync(filter).GetAwaiter().GetResult().ToListAsync();
+        }
+    }
+}
