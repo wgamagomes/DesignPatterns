c608deb3585a5d44a5db74ad42c9ee0be2634927
wgamagomes <wgamagomes@gmail.com>
2019-10-09 15:40:35 -0300

    [ADD] IGenericRepository and  GenericRepository class

---
+++ XPTO/Mongo/Infra.Data/Interfaces/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Infra.Data.Interfaces
+{
+    public interface IGenericRepository<TDocument> where TDocument : Document
+    {
+        Task InsertAsync(TDocument document);
+
+        Task DeleteAsync(Expression<Func<TDocument, bool>> filter);
+
+        Task<TDocument> UpdateAsync(TDocument document);
+
+        Task<List<TDocument>> FindAsync();
+
+        Task<List<TDocument>> FindAsync(Expression<Func<TDocument, bool>> filter);
+    }
+}
