0195e57ea679f5c2e5bdc4d32ee7a08260a722f1
wgamagomes <wgamagomes@gmail.com>
2019-10-12 15:19:26 -0300

    [ADD] Added Perceptron project [ADD] Added Perceptron class [ADD] Added Program class

---
+++ XPTO/AI/ArtificialNeuralNetwork/ArtificialNeuralNetwork.Perceptron/Perceptron.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArtificialNeuralNetwork.Perceptron
+{
+    public class Perceptron
+    {
+        private int[] _inputs;
+        private double[] _weights;
+
+
+        public void SetInputs(params int[] inputs)
+        {
+            _inputs = inputs;
+        }
+
+        public void SetWeights(params double[] weights)
+        {
+            _weights = weights;
+        }
+
+
+        //Sum all of the weighted inputs
+        private double SumAllweighted()
+        {
+            double sum = 0;
+
+            for (int i = 0; i < _inputs.Length; i++)
+            {
+                //For every input, multiply that input by its weight.
+                sum += _inputs[i] * _weights[i];
+            }
+
+            return sum;
+        }
+
+        public int Guess()
+        {
+            //Compute the output of the perceptron based on that sum passed through an activation function (the sign of the sum)
+
+            var sum = SumAllweighted();
+
+            var output = Activate(sum);
+
+            return output;
+
+        }
+
+        private void RecalculateWeights()
+        {
+
+        }
+
+        private int Activate(double sum)
+        {
+            if (sum < 0)
+                return -1;
+
+            return 1;
+        }
+    }
+}
---
+++ XPTO/AI/ArtificialNeuralNetwork/ArtificialNeuralNetwork.Perceptron/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ArtificialNeuralNetwork.Perceptron
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Perceptron p;
+            p = new Perceptron();
+            p.SetInputs(12, 4);
+            p.SetWeights(0.5, -1);
+
+            var guess = p.Guess();
+        }
+    }
+}
