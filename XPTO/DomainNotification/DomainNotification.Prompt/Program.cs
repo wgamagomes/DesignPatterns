6e1f73ae73a6e00b5096b33171eecf5f5226840d
wgamagomes <wgamagomes@gmail.com>
2019-10-16 12:01:07 -0300

    [ADD] Added DomainNotification.Prompt.csproj

---
+++ XPTO/DomainNotification/DomainNotification.Prompt/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DomainNotification.Prompt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
