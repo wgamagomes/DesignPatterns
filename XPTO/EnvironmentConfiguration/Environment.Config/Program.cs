453bf0bff44748a6f469ca34a150d659d21a4959
wgamagomes <wgamagomes@gmail.com>
2019-10-28 13:11:27 -0300

    [ADD] Added Environment.Config project

---
+++ XPTO/EnvironmentConfiguration/Environment.Config/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Environment.Config
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
