83629c1d7abbbbca2ab9750bd891dd8e528e75df
wgamagomes <wgamagomes@gmail.com>
2019-10-29 17:43:46 -0300

    [ADD] Added EnvironmentConfigBase class [ADD] Added EnvironmentVariableConstant class

---
+++ XPTO/EnvironmentConfiguration/Environment.Config/EnvironmentConfigBase.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+
+namespace Environment.Config
+{
+    public abstract class EnvironmentConfigBase
+    {
+        protected EnvironmentConfigBase NextEnvironment { get; set; }
+
+        public abstract IConfigurationBuilder Configure();
+
+        public string GetEnvironmentVariable(string variable)
+        {
+            return System.Environment.GetEnvironmentVariable(variable);
+        }
+    }
+}
---
+++ XPTO/EnvironmentConfiguration/Environment.Config/EnvironmentVariableConstant.cs
@@ -0,0 +1,18 @@
+﻿namespace Environment.Config
+{
+    public static class EnvironmentVariableKey
+    {
+        public const string APPLICATION_ENVIRONMENT = "APPLICATION_ENVIRONMENT";
+        public const string HOMOL_CONFIG = "HOMOL_CONFIG";
+        public const string PROD_CONFIG = "PROD_CONFIG";
+    }
+
+
+    public static class EnvironmentVariableValue
+    {
+        public const string DEV = "DEV";
+        public const string HOMOL = "HOMOL";
+        public const string PROD = "PROD";
+      
+    }
+}
